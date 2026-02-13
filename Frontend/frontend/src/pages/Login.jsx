0069f1779b830e893ee45446a9ca29da9f005900
CodeOmari <jacksonomari2230@gmail.com>
2026-02-13 18:54:09 +0300

    Update the route for the login page

--- Frontend/frontend/src/pages/Login.jsx
+++ Frontend/frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import Form from "../components/Form"
 
 export default function Login() {
-    return <Form route="/login/" method="login" />
+    return <Form route="/api/token/" method="login" />
 }
