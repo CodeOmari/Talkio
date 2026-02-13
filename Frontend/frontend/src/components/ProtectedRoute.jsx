c273d0fe7e03d0a92e3b0355b7d1cf51839d6fd8
CodeOmari <jacksonomari2230@gmail.com>
2026-02-13 18:53:10 +0300

    update the refresh token url

--- Frontend/frontend/src/components/ProtectedRoute.jsx
+++ Frontend/frontend/src/components/ProtectedRoute.jsx
@@ -16,8 +16,8 @@ export default function ProtectedRoute({ children }) {
     const refreshToken = async () => {
         const refreshToken = localStorage.getItem(REFRESH_TOKEN);
         try {
-            const res = await api.post("/token/refresh/", {
-                refresh: refreshToken,
+            const res = await api.post("api/token/refresh/", {
+                refresh_token: refreshToken,
             });
             if (res.status === 200) {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access_token)
