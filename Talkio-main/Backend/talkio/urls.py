24ba2dcd00625d642b01f9340369ca5c3c4cf41f
CodeOmari <jacksonomari2230@gmail.com>
2026-02-24 20:04:50 +0300

    update  searrch user and message urls

--- Talkio-main/Backend/talkio/urls.py
+++ Talkio-main/Backend/talkio/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path("my-messages/<user_id>/", views.ChatInbox.as_view(), name='my_inbox'),
-    path("get-messages/<sender_id>/<receiver_id>/", views.GetChat.as_view(), name='get_messages'),
+    path("my-messages/<int:user_id>/", views.ChatInbox.as_view(), name='my_inbox'),
+    path("get-messages/<int:sender_id>/<int:receiver_id>/", views.GetChat.as_view(), name='get_messages'),
     path("send-message/", views.SendMessage.as_view(), name='send_messages'),
 
     
     path("profile/<int:pk>/", views.ProfileDetail.as_view(), name='profile'),
-    path("search/<username>/", views.SearchUser.as_view(), name='search_user'),
+    path("search/<str:username>/", views.SearchUser.as_view(), name='search_user'),
 ]
