101c0d9c0c258cc8ca8f8dc0bddd37ac4bc6b28f
CodeOmari <jacksonomari2230@gmail.com>
2026-02-24 19:54:33 +0300

    add MessageDetail and SerachUsers imports from respective pages

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Messages from "./pages/Messages"
+import MessageDetail from "./pages/MessageDetail"
+import SearchUsers from "./pages/SearchUser"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
 
@@ -31,6 +33,8 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<RegisterAndLogout />} />
+        <Route component={MessageDetail} path="/inbox/:id" exact />
+        <Route component={SearchUsers} path="/search/:username" exact />
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
