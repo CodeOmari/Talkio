0baf89dc68df4e0a10880edbdb016d1f9a64dbdb
CodeOmari <jacksonomari2230@gmail.com>
2026-06-29 13:05:26 +0300

    add URL Routing configuration for the user_management app

---
+++ backend/user_management/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from .views import UserCreateView, current_user
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+urlpatterns = [
+    path('user/register/', UserCreateView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+
+    path('user/', current_user),
+    
+    path('api-auth/', include('rest_framework.urls')),
+]
